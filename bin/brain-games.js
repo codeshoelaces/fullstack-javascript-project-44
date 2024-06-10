d640a446836bddd0388212686e4b19d60ef1cc71
Shoe Laces <code.shoelaces@gmail.com>
2024-06-10 13:51:03 +0400

    Final

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,12 +2,12 @@
 
 import even from '../src/games/even.js';
 import calc from '../src/games/calc.js';
-import { game3 } from '../src/games/gcd.js';
-import { game4 } from '../src/games/progression.js';
-import { game5 } from '../src/games/prime.js';
+import gcd from '../src/games/gcd.js';
+import progression from '../src/games/progression.js';
+import prime from '../src/games/prime.js';
 
 even();
 calc();
-game3();
-game4();
-game5();
+gcd();
+progression();
+prime();
